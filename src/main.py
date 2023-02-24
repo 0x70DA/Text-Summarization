6024fa150da468907dd93c29d11bce45e96b9d29
Mahmoud Hussein <mahmoudh3257@gmail.com>
2023-02-24 23:07:11 +0200

    Remove detect last checkpoint

--- src/main.py
+++ src/main.py
@@ -18,7 +18,6 @@ from transformers import (
     TFAutoModelForSeq2SeqLM,
     create_optimizer,
 )
-from transformers.trainer_utils import get_last_checkpoint
 
 from utils import DataTrainingArguments, ModelArguments, TFTrainingArguments
 
@@ -86,27 +85,6 @@ def main():
         )
     # endregion
 
-    # region Detecting last checkpoint
-    last_checkpoint = None
-    if (
-        os.path.isdir(training_args.output_dir)
-        and not training_args.overwrite_output_dir
-    ):
-        last_checkpoint = get_last_checkpoint(training_args.output_dir)
-
-        if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
-            raise ValueError(
-                f"Output directory ({training_args.output_dir}) already exists and is not empty. "
-                "Use --overwrite_output_dir to overcome."
-            )
-        elif (
-            last_checkpoint is not None and training_args.resume_from_checkpoint is None
-        ):
-            logger.info(
-                f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
-                "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
-            )
-    # endregion
 
     # region Load dataset
     raw_datasets = load_dataset(
